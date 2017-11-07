a72b9070544ce8b9505020aed065b1f970fe662c
何星辰 <946834832@qq.com>
2017-11-08 00:23:08 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('koa-router')()
 const { query } = require('../db/async-db')
 router.get('/report', async(ctx, next) => {
-    let dataList = await query('SELECT * FROM TEST')
+    let dataList = await query('SELECT * FROM test')
     console.log(dataList)
     ctx.body = {
         code: 0,
